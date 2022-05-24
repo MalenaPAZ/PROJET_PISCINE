11dfb5deee4fe63da52f6a2c80ba13126757575b
Malena <malena.paz@edu.ece.fr>
2022-05-24 12:46:32 +0200

    delte

--- Login.php
+++ Login.php
@@ -10,11 +10,13 @@ $database = "omnes sante";
 $db_handle = mysqli_connect('localhost', 'root', '');
 $db_found = mysqli_select_db($db_handle, $database);
 //*************************************
+
+
 // si bouton1 (Rechercher) est cliqué
 if (isset($_POST["button1"])){
 if ($db_found) {
 //commencer le query
-$sql = "SELECT * FROM admin";
+$sql = "SELECT * FROM medecin ";
 if (($login != "") && ($mdp != "")) {
 //on recherche le livre par son titre
 $sql .= " WHERE Login LIKE '%$login%' AND Mdp LIKE '%$mdp%'";
@@ -35,6 +37,74 @@ echo "/// Compte trouvé";
 echo "<p>Database not found.</p>";
 }
 } //end Rechercher
+
+
+
+//si le bouton2 (Ajouter) est cliqué
+if (isset($_POST["button2"]))   {
+    if ($db_found) {
+    //on cherche l'ADMIN
+    $sql = "SELECT * FROM admin ";
+    //avec son LOGIIN ET MDP
+    if (($login != "")&& ($mdp != "")) {
+    $sql .= " WHERE Login LIKE '%$login%' AND Mdp LIKE '%$mdp%'";
+    }
+    $result = mysqli_query($db_handle, $sql);
+    //regarder s'il y a de resultat
+    if (mysqli_num_rows($result) != 0) {
+    echo "<p>Compte already exists. Duplicates not allowed.</p>";
+    } else {
+    //on ajoute ce compte
+    $sql = "INSERT INTO admin (Mdp, Login)
+     VALUES('$mdp', '$login')";
+    $result =mysqli_query($db_handle, $sql);
+        if($result)
+        {
+            echo "Insert Sucessful";
+        }
+        else {
+            echo "Unable to insert";
+            }    
+    
+    
+}}}
+
+
+
+//si le bouton3 (Supprimer) est cliqué
+if (isset($_POST["button3"])) {
+    if ($db_found) {
+    //on cherche le livre
+    $sql = "SELECT * FROM admin";
+    //avec son titre et auteur
+    if (($login != "")&&($mdp != "")) {
+    $sql .= " WHERE Login LIKE '%$login%'AND Mdp LIKE '%$mdp%'";
+    }
+    $result = mysqli_query($db_handle, $sql);
+    //regarder s'il y a de resultat
+    if (mysqli_num_rows($result) == 0) {
+        //ce livre n'existe pas
+    echo "<p>Cannot delete. account not found.</p>";
+    } else {
+    //on supprime cet item
+    while ($data = mysqli_fetch_assoc($result)) {
+        $id = $data['ID'];
+        }
+    //on supprime cet item par son ID
+    $sql = "DELETE FROM admin WHERE ID = $id";
+    $result =mysqli_query($db_handle, $sql);
+    
+    if($result)
+        {
+            echo "delete Sucessful";
+        }
+        else {
+            echo "Unable to delete";
+            }    
+    //on affiche le reste des livres dans notre BDD
+    
+}}}
+
 
 
 
