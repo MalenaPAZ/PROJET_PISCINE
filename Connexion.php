47828c5d6149d464a8f0d38f41b812c6e09f8a00
maxthd <maxthd@gmail.com>
2022-05-27 15:39:15 +0200

    connexion

--- Connexion.php
+++ Connexion.php
@@ -171,6 +171,53 @@ session_start();
         
     </footer><!-- End Footer -->
 
+    <?php
+        $database = "omnes sante";
+        $db_handle = mysqli_connect('localhost','root','');
+        $db_found = mysqli_select_db($db_handle, $database);
+        $login = isset($_POST["login"])? $_POST["login"]: "";
+        $mdp = isset($_POST["mdp"])? $_POST["mdp"]: "";
+
+
+
+        //si le bouton est cliqué
+        if (isset($_POST["button1"])) {
+        //traitement
+        if ($db_found) {
+            $_SESSION["login"] = $login;
+            $_SESSION["mdp"] =  $mdp;
+
+        $sql = "SELECT * FROM patient WHERE Login LIKE '$login' AND
+        Mdp LIKE '$mdp'";
+        $result = mysqli_query($db_handle, $sql);
+        if (mysqli_num_rows($result) == 0)
+        {
+            $sql = "SELECT * FROM medecin WHERE Login LIKE '$login' AND Mdp LIKE '$mdp'";
+            $result = mysqli_query($db_handle, $sql);
+            if (mysqli_num_rows($result) == 0)
+            {
+                $sql = "SELECT * FROM admin WHERE Login LIKE '$login' AND Mdp LIKE '$mdp'";
+                $result = mysqli_query($db_handle, $sql);
+                if (mysqli_num_rows($result) == 0)
+                {
+                    echo "Compte incorrect";
+
+                } else { 
+                    
+                    header("Location: accueilAdmin.php");}//si admin
+            }else { 
+                
+                header("Location: accueilMed.php");}//si medecin
+        }else { 
+            
+            header("Location: accueilPat.php");}//si patient
+        } 
+        echo "BDD not found";
+
+        //fermer la connexion
+        mysqli_close($db_handle);
+        }
+        ?>
 
     </body>
 </html>
