d46125ebe80c3abdd03eea11c33a51875a623131
Malena <malena.paz@edu.ece.fr>
2022-05-28 12:31:18 +0200

    creerptient

--- CreerPatient.php
+++ CreerPatient.php
@@ -73,10 +73,52 @@ if (isset($_POST["Creer"])) {
         } else {
 
             //on ajoute ce compte
-            $sql = "INSERT INTO patient (NomPatient, PrenomPatient, Login, Mdp, EmailPatient, NumPatient, AdressePatient,NumCarteVitale,SpecialitePref, Ville, CodePostal, Pays, Photo)
+            $sql1 = "INSERT INTO patient (NomPatient, PrenomPatient, Login, Mdp, EmailPatient, NumPatient, AdressePatient,NumCarteVitale,SpecialitePref, Ville, CodePostal, Pays, Photo)
      VALUES('$nom','$prenom','$login','$motdepasse','$email','$telephone','$adresse','$numcartevitale','$specialiteP','$ville','$codepostal','$pays','$avatar')";
-            $result = mysqli_query($db_handle, $sql);
+            $result1 = mysqli_query($db_handle, $sql1);
         }
+        //on cherche l'ADMIN
+        $sql = "SELECT * FROM patient ";
+        //avec son LOGIIN ET MDP
+        if (($login != "") && ($motdepasse != "")) {
+            $sql .= " WHERE Login LIKE '%$login%' AND Mdp LIKE '%$motdepasse%'";
+        }
+        $result = mysqli_query($db_handle, $sql);
+
+        ## Définitions des deux constantes
+        define('ADRESSE_WEBMASTER', 'pazmalena2001@yahoo.fr'); // Votre adresse qui apparaitra en tant qu'expéditeur des E-mails
+        define('SUJET', 'Creation Compte Omnes sante'); // Sujet commun aux deux E-mail
+
+        ## Message envoyé au visiteur
+        $message = "Bonjour ".$prenom.", Bienvenue hihihi.
+        \nCeci est un mail automatique, Merci de ne pas y répondre.
+         \n\nL'équipe Omnes Sante";
+
+        ## Second appel de la fonction mail() : le visiteur reçoit cet E-mail
+        ini_set('SMTP', 'smtp.orange.fr'); //il faut mettre le stmp qui correspond à son serveur, le lien suivant nous le donne : http://check414.free.fr/detection-smtp/
+        ini_set("sendmail_from", "pazmalena2001@yahoo.fr"); //donne l'expéditeur (il faut mettre une vrai addresse mail)
+        mail($email, SUJET, $message, 'From: Omnes sante' ); //on envoie le mail
+
+
+        if (mail($email, SUJET, $message, 'From: Omnes sante' )) {
+
+            echo'<script type="text/javascript">
+            alert("email de validation de creation de compte envoyé à '.$email .'")';
+            
+            echo '</script>';
+            
+           
+
+        } else {
+    
+            echo'<script type="text/javascript">
+            alert("Impossible de creer le compte")';
+            echo '</script>';
+            //header("Location : CreerPatient.html");
+    
+        }
+
+        header("Location : Accueil_neutre.html");
     } else echo "database not found";
 } else echo "bouton not found";
 
